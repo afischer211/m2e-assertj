08b52fb92388561e3a56ddcc5060f9460f50f83e
Jörg Henne <j.henne@levigo.de>
2016-07-21 14:56:53 +0200

    Fix output location - use target/test-classes instead of target/classes

--- org.levigo.m2e.assertj/src/org/levigo/m2e/assertj/internal/AssertJProjectConfigurator.java
+++ org.levigo.m2e.assertj/src/org/levigo/m2e/assertj/internal/AssertJProjectConfigurator.java
@@ -6,11 +6,21 @@
 
 package org.levigo.m2e.assertj.internal;
 
+import java.io.File;
+
 import org.apache.maven.plugin.MojoExecution;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jdt.core.IClasspathAttribute;
+import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.m2e.core.lifecyclemapping.model.IPluginExecutionMetadata;
 import org.eclipse.m2e.core.project.IMavenProjectFacade;
 import org.eclipse.m2e.core.project.configurator.AbstractBuildParticipant;
+import org.eclipse.m2e.core.project.configurator.ProjectConfigurationRequest;
 import org.eclipse.m2e.jdt.AbstractSourcesGenerationProjectConfigurator;
+import org.eclipse.m2e.jdt.IClasspathDescriptor;
+import org.eclipse.m2e.jdt.IClasspathEntryDescriptor;
 
 public class AssertJProjectConfigurator extends AbstractSourcesGenerationProjectConfigurator {
   @Override
@@ -22,5 +32,26 @@ public class AssertJProjectConfigurator extends AbstractSourcesGenerationProject
   @Override
   protected String getOutputFolderParameterName() {
     return "targetDir";
+  }
+  
+  // super implementation needs to be overridden just to use the test output location instead of the main one
+  public void configureRawClasspath(ProjectConfigurationRequest request, IClasspathDescriptor classpath,
+      IProgressMonitor monitor) throws CoreException {
+    IMavenProjectFacade facade = request.getMavenProjectFacade();
+
+    assertHasNature(request.getProject(), JavaCore.NATURE_ID);
+
+    for(MojoExecution mojoExecution : getMojoExecutions(request, monitor)) {
+      File[] sources = getSourceFolders(request, mojoExecution, monitor);
+
+      for(File source : sources) {
+        IPath sourcePath = getFullPath(facade, source);
+
+        if(sourcePath != null) {
+          IClasspathEntryDescriptor entry = classpath.addSourceEntry(sourcePath, facade.getTestOutputLocation(), true);
+          entry.setClasspathAttribute(IClasspathAttribute.IGNORE_OPTIONAL_PROBLEMS, "true"); //$NON-NLS-1$
+        }
+      }
+    }
   }
 }
